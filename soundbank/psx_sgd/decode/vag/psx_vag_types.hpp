65ce716ac9e125fb74943e8f980f88019a7d87ed
Cheena <97012270+Nezogoku@users.noreply.github.com>
2022-10-09 20:29:29 -0400

    Update psx_vag_types.hpp

--- soundbank/psx_sgd/decode/vag/psx_vag_types.hpp
+++ soundbank/psx_sgd/decode/vag/psx_vag_types.hpp
@@ -5,6 +5,8 @@
 
 #define SAMPLE_BYTES 14
 #define SAMPLE_NIBBLE SAMPLE_BYTES * 2
+#define SHORT_SAMPLE_BYTES 3
+#define SHORT_SAMPLE_NIBBLE SHORT_SAMPLE_BYTES * 2
 
 
 ///Flags for VAG flag byte
@@ -14,17 +16,24 @@ enum flags {
     LOOP_REGION = 2,     /* Loop region*/
     LOOP_END = 3,        /* Ending block of the loop */
     LOOP_FIRST_BLOCK = 4,/* First block of looped data */
-    UNK = 5,             /* ?*/
+    UNK = 5,             /* Ending position?*/
     LOOP_START = 6,      /* Starting block of the loop*/
     PLAYBACK_END = 7     /* Playback ending position */
 };
 
 ///VAG chunks
-struct chunk {
+struct vchunk {
     uint8_t shift;
     uint8_t predict;
     uint8_t flag;
     uint8_t data[SAMPLE_BYTES];
+};
+
+///VGS chunks
+struct gchunk {
+    uint8_t shift;
+    uint8_t predict;
+    int16_t data[SAMPLE_NIBBLE];
 };
 
 
