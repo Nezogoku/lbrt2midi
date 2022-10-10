1fb55e2ba958caf6a02686a5fdd651f0af0a7621
Cheena <97012270+Nezogoku@users.noreply.github.com>
2022-10-09 20:28:47 -0400

    Update decode_utils.hpp

--- soundbank/psx_sgd/decode/decode_utils.hpp
+++ soundbank/psx_sgd/decode/decode_utils.hpp
@@ -1,16 +1,53 @@
 #ifndef DECODE_UTILS_HPP
 #define DECODE_UTILS_HPP
 
-#include <cstdint>      //For uintX_t, intX_t
+#include <cstring>      //For std::strcmp()
 
 
-///Gets char array from vector
-char* getChars(char *data, int pos, int len) {
-    char* out = new char[len];
+///Compares chars from vector to char array
+bool cmpChars(char *data, char str1[], int pos, int len) {
+    char str0[len + 1];
+    for (int p = 0; p < len; ++p) str0[p] = data[pos + p];
+    return !strcmp(str0, str1);
+}
 
-    for (int s = 0; s < len; ++s) out[s] = data[pos + s];
+///Gets variable chars from vector
+void getChars(char *data, char *out, int pos, int len) {
+    for (int p = 0; p < len; ++p) out[p] = data[pos + p];
 
-    return out;
+    pos += len;
+}
+
+///Gets variable bits from vector
+void getBits(char *data, int &out, int pos, int len, int &shift) {
+    out = 0;
+    for (int b = 0; b < len; ++b) {
+        out <<= 1;
+        out |= (data[pos] >> shift++) & 0x01;
+        if (shift == 8) { shift = 0; pos += 1; }
+    }
+}
+
+///Gets LE variable int from vector
+void getLeInt(char *data, int &out, int pos, int len) {
+    out = 0;
+    for (int s = len; s > 0; --s) {
+        out <<= 8;
+        out |= data[pos + (s - 1)];
+    }
+
+    pos += len;
+}
+
+///Gets BE variable int from vector
+void getBeInt(char *data, int &out, int pos, int len) {
+    out = 0;
+    for (int s = 0; s < len; ++s) {
+        out <<= 8;
+        out |= data[pos + s];
+    }
+
+    pos += len;
 }
 
 
