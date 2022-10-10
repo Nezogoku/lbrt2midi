76ebedcdccf720452bba874157cbc07b8bc6c98d
Cheena <97012270+Nezogoku@users.noreply.github.com>
2022-10-09 20:34:05 -0400

    Update atrac.cpp

--- soundbank/psx_sgd/decode/atrac/atrac.cpp
+++ soundbank/psx_sgd/decode/atrac/atrac.cpp
@@ -3,6 +3,7 @@
 #include <cstdint>      //For uintX_t, intX_t, INT16_MIN, INT16_MAX
 #include <vector>       //For std::vector
 #include "../decode.hpp"
+#include "../decode_utils.hpp"
 #include "atrac_tables.hpp"
 
 using std::round;
@@ -23,6 +24,13 @@ std::vector<int16_t> at3Decode(char *at3Data, uint32_t at3Size, int ch, uint32_t
 std::vector<int16_t> atpDecode(char *atpData, uint32_t atpSize, int ch, uint32_t rt) {
 	vector<char> rawData;
 
+	if (!cmpChars(atpData, "RIFF", 0,  4) ||
+        !cmpChars(atpData, "WAVE", 8,  4) ||
+        !cmpChars(atpData, "fmt ", 12, 4)) return {};
+
+    int format;
+    getLeInt(atpData, format, 20, 2);
+    format = ~format;
 
     return at3Decode(rawData.data(), rawData.size(), ch, rt);
 }
