e94aba7e1392c4c468d2bc12704d006b937c8cce
Cheena <97012270+Nezogoku@users.noreply.github.com>
2022-11-17 12:56:19 -0500

    Update decode.hpp
    Removed unnecessary functions

--- soundbank/psx_sgd/decode/decode.hpp
+++ soundbank/psx_sgd/decode/decode.hpp
@@ -5,23 +5,11 @@
 #include <vector>       //For std::vector
 
 
-///Decodes header-less PCMBE files
-std::vector<int16_t> pcmbDecode(char *pcmbData, uint32_t pbeSize, uint32_t &loopStart, uint32_t &loopEnd);
-
 ///Decodes OGG-VORBIS files
 std::vector<int16_t> oggDecode(char *oggData, uint32_t oggSize, int ch, uint32_t rt, uint32_t &loopStart, uint32_t &loopEnd);
 
 ///Decodes header-less VAG files
 std::vector<int16_t> vagDecode(char *vagData, uint32_t vagSize, uint32_t &loopStart, uint32_t &loopEnd);
-
-///Decodes AT3p files
-std::vector<int16_t> atpDecode(char *atpData, uint32_t atpSize, int ch, uint32_t rt);
-
-///Decodes header-less short VAG files
-std::vector<int16_t> vgsDecode(char *vgsData, uint32_t vgsSize, uint32_t &loopStart, uint32_t &loopEnd);
-
-///Decodes header-less AT3 files
-std::vector<int16_t> at3Decode(char *at3Data, uint32_t at3Size, int ch, uint32_t rt);
 
 
 #endif
