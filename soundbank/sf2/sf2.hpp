60a5e4b3e73d9c224f610b4cdf63dfa87e9f6ceb
Cheena <97012270+Nezogoku@users.noreply.github.com>
2023-02-17 16:25:04 -0500

    Delete soundbank directory

--- soundbank/sf2/sf2.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef SF2_HPP
-#define SF2_HPP
-
-int setSF2(std::string sgd_file, std::string &sf2_file, bool isDebug);
-int setSF2(std::string sgh_file, std::string sgb_file, std::string &sf2_file, bool isDebug);
-
-#endif
