2c1d2e6248b7a707533ca2fb7a5f01ca5199974c
Cheena <97012270+Nezogoku@users.noreply.github.com>
2024-01-30 14:34:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,125 +1,94 @@
+// Base codes in playmidi from schellingb
+// Based off LBRTPlayer from owocek
+//      Used for comparison stuffs
+
 #include <algorithm>
 #include <cstdio>
 #include <string>
-#include "soundbank/sf2/sf2.hpp"
-#include "soundbank/psx_sgd/psxsgd.hpp"
-#include "sequence/lrt/lrt.hpp"
-#include "sequence/mid/playmidi.hpp"
+#include "playmidi/playmidi.hpp"
+#include "pspimg/pspimg.hpp"
+#include "printpause.hpp"
 
-// SF2cute library from gocha
-// TinySoundFont library and base codes in sequence/mid from schellingb
-// Based off LBRTPlayer from owocek
-//      Used for comparison stuffs
-// Base codes in decode/vag from jmarti856 (I basically just changed the language it was written in)
-//                               and vgmstream (rewritten to resemble jmarti856's)
-// stb_vorbis library and base codes in decode/ogg from Sean Barrett
+#define PROGRAMME_IDENTIFIER "lbrt2midi v5.0"
 
 
-void printOpt(std::string pName, bool debug) {
+void printOpt(const char *pName, bool debug) {
     if (debug) fprintf(stderr, "Not enough valid files entered\n");
 
-    fprintf(stderr, "Usage: %s [options] [<infile(s).sgd/sf2>] [<infile(s).lrt/mid>]\n\n", pName.c_str());
+    fprintf(stderr, "Usage: %s [options] [<infile.sf2>] [<infile(s).lrt/mid>]\n\n", pName);
     fprintf(stderr, "Options:\n");
     fprintf(stderr, "   -h          Prints this message\n");
     fprintf(stderr, "   -d          Toggles debug mode\n");
     fprintf(stderr, "   -p          Activates playback mode\n");
-    fprintf(stderr, "                   Uses last (converted) SF2 and all (converted) MIDI's\n");
+    fprintf(stderr, "                   Uses most recent SF2 and all (converted) MIDI's\n");
 }
 
 
 int main(int argc, char *argv[]) {
-    bool debug = false,
-         play_result = false;
+    bool debug = false, play_result = false;
 
     std::string prgm = argv[0];
     prgm.erase(std::remove(prgm.begin(), prgm.end(), '\"'), prgm.end());
     prgm = prgm.substr(prgm.find_last_of("\\/") + 1);
     prgm = prgm.substr(0, prgm.find_last_of('.'));
 
-    std::string playables[argc] = {};
-
-    if (argc < 2) {
-        printOpt(prgm, debug);
-        return 0;
-    }
+    if (argc < 2) { printOpt(prgm.c_str(), debug); }
     else {
-        std::string tempFile = "";
+        playmidi player(argc - 1);
 
-        for (int i = 1, p = 1; i < argc; ++i) {
-            tempFile = argv[i];
-            tempFile.erase(std::remove(tempFile.begin(), tempFile.end(), '\"'), tempFile.end());
+        for (int i = 1; i < argc; ++i) {
+            std::string tfle, ext, fle;
 
-            if (tempFile == "-h") {
-                printOpt(prgm, debug);
-                return 0;
-            }
-            else if (tempFile == "-d") {
-                debug = !debug;
-                continue;
-            }
-            else if (tempFile == "-p") {
-                play_result = true;
-                continue;
-            }
+            tfle = argv[i];
+            tfle.erase(std::remove(tfle.begin(), tfle.end(), '\"'), tfle.end());
 
+            if (tfle == "-h") { printOpt(prgm.c_str(), debug); break; }
+            else if (tfle == "-d") { debug = !debug; player.setDebug(debug); continue; }
+            else if (tfle == "-p") { play_result = true; continue; }
+            else if (tfle.find('.') == std::string::npos) continue;
 
-            std::string ext = tempFile.substr(tempFile.size() - 4),
-                        fle = tempFile.substr(0, tempFile.find_last_of('.'));
+            ext = tfle.substr(tfle.find_last_of('.') + 1);
+            fle = tfle.substr(0, tfle.find_last_of('.'));
 
-            if (debug) fprintf(stdout, "\nFile base name: %s\n", fle.substr(fle.find_last_of("\\/") + 1).c_str());
-            if (debug) fprintf(stdout, "File extension: %s\n", ext.substr(ext.find_last_of('.') + 1).c_str());
+            fprintf(stdout, "\n");
+            if (debug) fprintf(stderr, "File base name: %s\n", fle.substr(fle.find_last_of("\\/") + 1).c_str());
+            if (debug) fprintf(stderr, "File extension: %s\n", ext.c_str());
 
-            bool isSGD = (ext == ".sgd"),
-                 isSGH = (ext == ".sgh"),
-                 isSGB = (ext == ".sgb"),
-                 isSF2 = (ext == ".sf2"),
-                 isLRT = (ext == ".lrt"),
-                 isMID = (ext == ".mid") || (ext == ".smf");
+            bool isSF2 = (ext.find("sf2") != std::string::npos),
+                 isLRT = (ext.find("lrt") != std::string::npos),
+                 isMID = (ext.find("mid") != std::string::npos) ||
+                         (ext.find("smf") != std::string::npos);
 
             if (isLRT || isMID) {
-                if (debug) fprintf(stdout, "This is a sequenced file\n");
+                if (debug) fprintf(stderr, "\nThis is a sequenced file\n");
 
-                std::string endMid;
+                if (isMID) player.setSequence(tfle, i - 1);
+                else {
+                    lbrt pataMID(debug);
 
-                if (isMID) endMid = tempFile;
-                else if (isLRT && readLRT(tempFile, endMid, debug)) {}
-                else continue;
+                    if (!pataMID.setLRT(tfle)) continue;
+                    else if (!play_result && !pataMID.writeMidi()) continue;
 
-                int cnt = std::count(playables + 1, playables + argc, endMid);
-                if (!cnt) {
-                    playables[p++] = endMid;
-                    fprintf(stdout, "Sequence successfully saved\n");
+                    std::string mid_name = fle.substr(fle.find_last_of("\\/") + 1);
+                    unsigned char *mid_data = 0;
+                    unsigned mid_size = 0;
+                    pataMID.getMidi(mid_size, mid_data);
+
+                    player.setSequence(mid_size, mid_data, mid_name, i - 1);
                 }
             }
-            else if (isSGD || isSGH || isSGB || isSF2) {
-                if (debug) fprintf(stdout, "This is a soundbank file\n");
+            else if (isSF2) {
+                if (debug) fprintf(stderr, "\nThis is a soundbank file\n");
 
-                std::string sgh = fle + ".sgh", sgb = fle + ".sgb";
-
-                if (isSF2) playables[0] = tempFile;
-                else if (isSGD && setSF2(tempFile, debug)) playables[0] = tempFile;
-                else if ((isSGH || isSGB) && setSF2(sgh, sgb, debug)) playables[0] = sgh;
-                else continue;
+                player.setBank(tfle);
             }
         }
 
-
-        if (play_result) {
-            fprintf(stdout, "\nPlaying sequences\n");
-
-            playmidi player[argc] {};
-            for (int p = 1, s = 0; p < argc; ++p, ++s) {
-                if (playables[p].empty()) continue;
-
-                player[s] = playmidi(debug);
-
-                if (!player[s].setBank(playables[0])) break;
-                else if (!player[s].setSequence(playables[p])) continue;
-
-                player[s].playSequence();
-            }
-        }
+        if (play_result) player.playSequence();
     }
 
+    fprintf(stdout, "\nEnd of thing\n");
+
+    sleep(10);
     return 0;
 }
