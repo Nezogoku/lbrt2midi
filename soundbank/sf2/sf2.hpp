4a6a27cbdde968a7d9be12b8900c3caa20ea9d5a
Cheena <97012270+Nezogoku@users.noreply.github.com>
2024-01-30 14:25:13 -0500

    Removed soundbank\
    Removed the "soundbank" directory

--- soundbank/sf2/sf2.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SF2_HPP
-#define SF2_HPP
-
-#include <string>
-
-int setSF2(std::string &sgd_file, bool isDebug);
-int setSF2(std::string &sgh_file, std::string sgb_file, bool isDebug);
-
-#endif
